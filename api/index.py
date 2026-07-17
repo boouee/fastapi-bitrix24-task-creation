c58ce16967c3cd20561bb0a52a098c6576e7c00e
mdevelopeur <mdevelopeur@gmail.com>
2026-07-18 03:18:18 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -11,37 +11,13 @@ from urllib.parse import unquote, urlparse
 
 app = FastAPI()
 
-@app.post('/api/cartesia')
-async def cartesia_handler(request: Request):
-    try:
-        if request.headers.get("x-webhook-secret") != "secret":
-            data = {"error": "unauthorized"}
-            json_compatible_data = jsonable_encoder(data)
-    
-            # 2. Return JSONResponse with a custom status code
-            return JSONResponse(
-              status_code=status.HTTP_401_CREATED,
-              content=json_compatible_data
-            )
-        body = await request.body()
-        json = await request.json()
-        print(unquote(body))
-        data = unquote(body)
-        form_data = await request.form()
-        form_data = dict(form_data)
-        print(form_data)
-        
-    except Exception as e:
-        print(e)
-        traceback.print_exc()
-        return e
-        
 @app.post('/api/deal_in_stage')
 async def deal_in_stage_handler(request: Request):
     try:
         form_data = await request.form()
         form_data = dict(form_data)
         print(form_data)
+        print(form_data["document_id[2]"][5:])
     except Exception as e:
         print(e)
         traceback.print_exc()
