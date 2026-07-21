11317e3f5cab122ab430747fccb0e64c0480c564
boouee <104202645+boouee@users.noreply.github.com>
2026-07-21 22:47:09 +0500

    Delete api/__index.py

--- api/__index.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-# Initialize the FastAPI application instance
-app = FastAPI()
-
-# Define a root route that responds to HTTP GET requests
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
