dbc3c2bbe8d4a75175b7126b6211c75a6bbeb715
boouee <104202645+boouee@users.noreply.github.com>
2026-07-21 22:29:52 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,7 +8,6 @@ import re
 import traceback
 from urllib.parse import unquote, urlparse
 #from api.functions import main, get_preparations, get_deal_preparations
-from api.html import html
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
